cb53aedc10fbbac2ee1c7369b6aaa6a0c5bbf3b1
thainamtran <thainam83vn@gmail.com>
2021-11-21 09:27:37 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hi there 8!!!
+      </header>
+    </div>
+  );
+}
+
+export default App;
